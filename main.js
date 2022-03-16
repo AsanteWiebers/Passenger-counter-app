8a804a4c612fc9042f2cb4f8f241ad744c13a6f4
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-03-16 21:41:25 +0100

    save btn js

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
 let countEl = document.getElementById("count-el");
-
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
+
+
+console.log(saveEl);
+
+
+ 
 function increment() {
     
-    count = count + 1;
+    count += 1;
     countEl.innerText = count;
     console.log(count);
 }
 
 function decrement () {
-    count = count - 1;
+    count -= 1;
     countEl.innerText = count;
+    console.log(count);
 }
 
 function save() {
+    let countStr = count + " - ";
+    saveEl.innerText = countStr;
+
     console.log(count);
 }
+
+save();
+
+
+// let welcomeEl = document.getElementById("welcome-el");
+
+// let userName = "Asante";
+// let greeting = "welcome back";
+
+// welcomeEl.innerText = userName + ", " + greeting;
+// welcomeEl.innerText += " 👍🏿";
 
 
